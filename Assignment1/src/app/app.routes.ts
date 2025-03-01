8ef6baa1d64263fd6e29ac53f9f53a64fa6e2b14
MeetPatel08 <patelmeet52271@gmail.com>
2025-03-01 22:38:28 +0530

    Updated app components and added authentication module

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './auth/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AboutUsComponent } from './auth/about-us/about-us.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'about',component:AboutUsComponent},
+    {path:'forgot-password',component:ForgotPasswordComponent}
+];
